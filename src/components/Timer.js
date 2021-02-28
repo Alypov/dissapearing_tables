485097035693fcc77b688dcb6992db7d77d5b3e6
Vladimir Alypov <alypov.zp@gmail.com>
2021-02-28 21:33:24 +0100

    timer value after 00:00 fixed

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -10,37 +10,41 @@ const Timer = () => {
   const [minutes, setMinutes] = useState(initialMinutes);
   const [seconds, setSeconds] = useState(initialSeconds);
 
-  const [quit, setQuit] = useState(false);
-
   useEffect(() => {
-    if (seconds !== 0 && minutes !== 0) {
-      let interval = setInterval(() => {
-        if (seconds >= 1) {
-          setSeconds(seconds - 1);
+    let interval = setInterval(() => {
+      if (seconds >= 1) {
+        setSeconds(seconds - 1);
+        clearInterval(interval);
+        console.log(seconds);
+      }
+      if (seconds === 0) {
+        setSeconds(59);
+        clearInterval(interval);
+        if (minutes === 0) {
           clearInterval(interval);
-          console.log(seconds);
-        }
-        if (seconds === 0) {
-          setSeconds(59);
+        } else {
+          setMinutes(minutes - 1);
           clearInterval(interval);
-          if (minutes === 0) {
-            clearInterval(interval);
-          } else {
-            setMinutes(minutes - 1);
-            //   clearInterval(interval);
-          }
         }
-        if (seconds === 0 && minutes === 0) {
-          localStorage.clear();
-          clearInterval(interval);
-          window.location = '/';
-        }
-      }, 1000);
-    } else {
-      setQuit(true);
-    }
+      }
+      if (seconds <= 0 && minutes <= 0) {
+        clearInterval(interval);
+        setMinutes(0);
+        setSeconds(0);
+        localStorage.clear();
+        window.location = '/';
+      }
+    }, 1000);
   });
 
-  return <div>{quit ? <div>Time is over</div> : `${minutes}:${seconds}`}</div>;
+  return (
+    <div>
+      {seconds <= 0 && minutes <= 0 ? (
+        <div>Time is over</div>
+      ) : (
+        `${minutes}:${seconds}`
+      )}
+    </div>
+  );
 };
 export default Timer;
